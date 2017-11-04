eea777e0b58b09762b1a64e9b1e954de0cb60af6
Darren Le Minh Duc <leminhduc74@gmail.com>
2017-11-04 16:48:02 +0800

    Add accuracy_threshold comparison

--- src/LK_Track_Point.m
+++ src/LK_Track_Point.m
@@ -1,9 +1,12 @@
+% Return the best-guess velocity of point (x,y) on img1 to img2
+% To be used in a pyramid implementation
 function [u v] = LT_Track(img1, img2, win_size, x, y)
     % Constants
     d = [0 0];
     max_steps = 20;
     num_cols = size(img1,2);
     num_rows = size(img1,1);
+    accuracy_threshold = 0.05; % purely abbitrary now
     % Intensity derivative matrices
     Iy = double(uint8(zeros(num_rows, num_cols)));
     Ix = double(uint8(zeros(num_rows, num_cols)));
@@ -26,12 +29,17 @@ function [u v] = LT_Track(img1, img2, win_size, x, y)
     W_xy = conv2(gauss_kern, I_xy);
     Z = [W_xx(x,y) W_xy(x,y); W_xy(x,y) W_yy(x,y)];
     % Iterative improvement of estimation
+    % Either run for max_steps times or until error residual smaller than
+    % accuracy_threshold
     d_current = [0 0];
     for step = 1 : max_steps
         I_t = img1(x,y) - img2(x+d_current(1)+d(1), y+d_current(2)+d(2));
         b = [I_t*Ix(x,y);I_t*Iy(x,y)];
         guess = Z'*b;
         d_current = d_currebt + guess;
+        if (abs(guess) < accuracy_threshold)
+            break;
+        end
     end
     d = d_current;
     u = d(1);
