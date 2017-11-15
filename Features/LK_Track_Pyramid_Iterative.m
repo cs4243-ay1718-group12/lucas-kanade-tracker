363f0c2b8f7efcfabdd6fa16e4ff4e0c4bd541c1
Darren Wee <darren.wee@u.nus.edu>
2017-11-15 21:29:07 +0800

    refactor Maximum_Pyramid_Level

--- Features/LK_Track_Pyramid_Iterative.m
+++ Features/LK_Track_Pyramid_Iterative.m
@@ -1,11 +1,12 @@
 function [velo_x, velo_y] = LK_Track_Pyramid_Iterative(raw_img1, raw_img2, X, Y)
 
     % Constants
-    win_rad = 5;
-    accuracy_threshold = .01;
+    win_radius = 5;
+    accuracy_threshold = 0.001;
     max_iterations = 20;
-    max_levels = Maximum_Pyramid_Level(raw_img1, 128);
+    max_levels = get_max_pyramid_level(raw_img1, 128);
     num_points = size(X,1);
+    
     % Get images for each pyramid levels
     img1_pyramidized = generate_pyramid(raw_img1, max_levels);
     img2_pyramidized = generate_pyramid(raw_img2, max_levels);
@@ -26,22 +27,23 @@ function [velo_x, velo_y] = LK_Track_Pyramid_Iterative(raw_img1, raw_img2, X, Y)
         for point = 1 : num_points
             xt = U(point)*2;
             yt = V(point)*2;
-            [iX, iY, oX, oY, is_out_of_bound] = generate_window(xt, yt, win_rad, num_rows, num_cols);
-            if is_out_of_bound 
-                continue; 
-            end 
+            [iX, iY, oX, oY, is_out_of_bound] = generate_window(xt, yt, win_radius, num_rows, num_cols);
+            
+            if is_out_of_bound, continue; end
+            
             Ix = interp2(iX,iY,img1x(iY,iX),oX,oY);
             Iy = interp2(iX,iY,img1y(iY,iX),oX,oY);
             I1 = interp2(iX,iY,img1(iY,iX),oX,oY);
 
             for i = 1 : max_iterations
-                [iX, iY, oX, oY, is_out_of_bound] = generate_window(xt, yt, win_rad, num_rows, num_cols);
+                [iX, iY, oX, oY, is_out_of_bound] = generate_window(xt, yt, win_radius, num_rows, num_cols);
                 if is_out_of_bound, break; end
                 It = interp2(iX,iY,img2(iY,iX),oX,oY) - I1;
                 
                 vel = [Ix(:),Iy(:)]\It(:);
                 xt = xt+vel(1);
                 yt = yt+vel(2);
+                
                 if max(abs(vel)) < accuracy_threshold
                     break; 
                 end
